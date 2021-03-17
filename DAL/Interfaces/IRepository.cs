f35564681ec7b8e9d7422f7901bb024991c0b82d
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-17 16:18:05 +0200

    Add repository interface

---
+++ DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : IEntity<int>
+    {
+        public TEntity Add(TEntity entity);
+        public TEntity Update(TEntity entity);
+        public void Delete(TEntity entity);
+        public Task<TEntity> GetByIdAsync(int id);
+        public Task<TEntity> GetByIdWithDetailsAsync(int id);
+        public Task<TEntity> GetAllAsync();
+        public Task<TEntity> GetAllWithDetailsAsync();
+    }
+}
