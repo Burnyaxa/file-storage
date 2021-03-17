dafd4fc7053792467690cf85a2c16491d4e5f791
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-17 21:23:37 +0200

    Add UnitOfWork implementation

---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        public IRepository<File> FileRepository { get; }
+        public IRepository<FileStatistics> FileStatisticsRepository { get; }
+
+        public Task SaveAsync();
+    }
+}
