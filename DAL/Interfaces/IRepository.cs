faf55715a61870395cfda676e846ad3b6a2440cc
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-17 21:16:30 +0200

    Add Repository implementation

--- DAL/Interfaces/IRepository.cs
+++ DAL/Interfaces/IRepository.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Interfaces
 {
-    public interface IRepository<TEntity> where TEntity : IEntity<int>
+    public interface IRepository<TEntity> where TEntity : class, IEntity<int>
     {
         public TEntity Add(TEntity entity);
         public TEntity Update(TEntity entity);
         public void Delete(TEntity entity);
         public Task<TEntity> GetByIdAsync(int id);
-        public Task<TEntity> GetByIdWithDetailsAsync(int id);
-        public Task<IEnumerable<TEntity>> GetAllAsync();
-        public Task<IEnumerable<TEntity>> GetAllWithDetailsAsync();
+        public Task<TEntity> GetByIdWithDetailsAsync(int id, string includeProperties = "");
+        public IQueryable<TEntity> GetAll();
+        public IQueryable<TEntity> GetAllWithDetails(string includeProperties = "");
     }
 }
