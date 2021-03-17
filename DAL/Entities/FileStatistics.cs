8d66f27cfea0f269963958e9a72ce9b2b2ad127a
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-17 15:15:05 +0200

    Add FileStatistics entity

---
+++ DAL/Entities/FileStatistics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class FileStatistics : EntityBase<int>
+    {
+        public int Views { get; set; }
+        public int Downloads { get; set; }
+
+        public File File { get; set; }
+    }
+}
