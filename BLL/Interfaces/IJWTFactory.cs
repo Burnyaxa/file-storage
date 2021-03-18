d6ff6076b1251ffb14fcc70e31a71dde58ff4359
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-18 18:46:30 +0200

    Add JWTFactory interface

---
+++ BLL/Interfaces/IJWTFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace BLL.Interfaces
+{
+    public interface IJWTFactory
+    {
+        JwtSecurityToken DecodeToken(string token);
+        string GenerateEncodedToken(string userName, ClaimsIdentity identity);
+        Task<ClaimsIdentity> GenerateClaimsIdentity(User user);
+        int GetUserIdClaim(string token);
+        string GetUserRoleClaim(string token);
+    }
+}
