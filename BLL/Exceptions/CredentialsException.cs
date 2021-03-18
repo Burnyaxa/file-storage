c32dc85f186147735c480cde51801e5ecbf8c8d9
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-18 18:35:58 +0200

    Remove base constructor call

--- BLL/Exceptions/CredentialsException.cs
+++ BLL/Exceptions/CredentialsException.cs
@@ -8,7 +8,7 @@ namespace BLL.Exceptions
     {
         public override string Message { get; }
 
-        public CredentialsException() : base()
+        public CredentialsException()
         {
             Message = "Wrong credentials were entered";
         }
