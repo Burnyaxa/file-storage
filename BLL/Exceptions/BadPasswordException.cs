1572e31d8838ae0e5c7aeec15016e0192ea11715
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-18 18:26:27 +0200

    Add BadPassword exception

---
+++ BLL/Exceptions/BadPasswordException.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Exceptions
+{
+    public class BadPasswordException : BadRequestException
+    {
+        public override string Message { get; }
+
+        public BadPasswordException(IEnumerable<IdentityError> errors) : base()
+        {
+            Message = errors.Aggregate("", (msg, next) => msg + $"{next.Description} ");
+        }
+    }
+}
