90ebc07a8023773cb56ccf813835e9679e998efa
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-20 15:36:48 +0200

    Add auth dto and service interface

---
+++ BLL/DTO/AuthenticateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.DTO
+{
+    public class AuthenticateDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ BLL/Interfaces/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.DTO;
+
+namespace BLL.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthenticateDto> AuthenticateAsync(UserDto userDto);
+    }
+}
