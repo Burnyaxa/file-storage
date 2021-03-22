201ba1eb8b881eb9d62c3cb7e0dc34d837f24d81
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-22 20:23:26 +0200

    Add upload service

--- BLL/Interfaces/IFileUploadService.cs
+++ BLL/Interfaces/IFileUploadService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace BLL.Interfaces
 {
     public interface IFileUploadService
     {
-        Task<string> UploadFileAsync(Stream stream, string bucketName, string key);
+        Task<string> UploadFileAsync(IFormFile file, string bucketName);
     }
 }
---
+++ BLL/Services/FileUploadService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using BLL.Extensions;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace BLL.Services
+{
+    public class FileUploadService : IFileUploadService
+    {
+        private readonly RegionEndpoint _bucketRegion = RegionEndpoint.EUWest2;
+        private readonly IAmazonS3 _s3Client;
+
+        public FileUploadService(IAmazonS3 s3Client)
+        {
+            _s3Client = s3Client;
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile file, string bucketName)
+        {
+            string key = file.FileName + DateTime.Now;
+            var fileTransferUtility = new TransferUtility(_s3Client);
+            var uploadRequest = new TransferUtilityUploadRequest()
+            {
+                InputStream = file.OpenReadStream(),
+                Key = key,
+                BucketName = bucketName,
+                CannedACL = S3CannedACL.PublicRead
+            };
+
+            await fileTransferUtility.UploadAsync(uploadRequest);
+
+            return _s3Client.GetResourceUrl(bucketName, key);
+        }
+    }
+}
