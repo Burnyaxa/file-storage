dfd5decb747b9a4fd16a8c5d795ee539936a25c0
Artem Supryhan <suprigan.artem1@gmail.com>
2021-03-23 16:31:05 +0200

    Add statistics service

---
+++ BLL/Interfaces/IStatisticsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.DTO;
+
+namespace BLL.Interfaces
+{
+    public interface IStatisticsService
+    {
+        Task IncreaseViews(FileDto fileDto);
+        Task IncreaseDownloads(FileDto fileDto);
+    }
+}
---
+++ BLL/Services/StatisticsService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.DTO;
+using BLL.Exceptions;
+using BLL.Interfaces;
+using DAL.Interfaces;
+
+namespace BLL.Services
+{
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public StatisticsService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task IncreaseViews(FileDto fileDto)
+        {
+            var file = await _unitOfWork.FileRepository.GetByIdWithDetailsAsync(fileDto.Id);
+            if (file == null)
+            {
+                throw new EntityNotFoundException(nameof(file), fileDto.Id);
+            }
+
+            file.Statistics.Views++;
+            _unitOfWork.FileRepository.Update(file);
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task IncreaseDownloads(FileDto fileDto)
+        {
+            var file = await _unitOfWork.FileRepository.GetByIdWithDetailsAsync(fileDto.Id);
+            if (file == null)
+            {
+                throw new EntityNotFoundException(nameof(file), fileDto.Id);
+            }
+
+            file.Statistics.Downloads++;
+            _unitOfWork.FileRepository.Update(file);
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
